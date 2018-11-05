21e0342ab951393d10c746fafb518888055f6a2c
Aspen James <aspenjames@tqca.org>
2018-11-05 13:09:47 -0800

    init App container, add Home component and route, reduce header to 10%vh

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to Espresso.Log!</h2>
+      <p>Create an account or login to start tracking your espresso recipes.</p>
+
+      <h3>Notes for usage:</h3>
+      <ul>
+        <li>Usage notes will go here</li>
+        <li>Explanations of types of accounts</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import Home from '../components/Home';
 
 class App extends Component {
   render() {
@@ -8,18 +12,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        </header> <br />
+        <Router>
+          <React.Fragment>
+            {/* Navbar */}
+            <Route exact path='/' component={Home} />
+          </React.Fragment>
+        </Router>
       </div>
     );
   }
