85b0ac20fc769ac4cccd90a964852b1ed7e45724
Aspen James <aspenjames@tqca.org>
2018-11-05 19:11:06 -0800

    add registration form and styling

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+
+class RegistrationForm extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirmation: ''
+  }
+
+  componentWillUnmount() {
+    this.props.clearErrors();
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h3>Create your personal account</h3>
+
+        {this.props.errors && <p className='error'>{this.props.errors.join(', ')}</p>}
+
+        <form onSubmit={this.handleSubmit}>
+          <div className='formElement'>
+            <input type='text' id='name'
+              placeholder='Name'
+              value={this.state.name}
+              onChange={this.handleChange} />
+            <p className='light small'>This will act as your public username. Feel free to use your full name or a handle.</p>
+
+            <input type='email' id='email'
+              placeholder='Email'
+              value={this.state.email}
+              onChange={this.handleChange} />
+            <p className='light small'>You'll use this to log in once your account is created.</p>
+
+            <input type='password' id='password'
+              placeholder='Password'
+              value={this.state.password}
+              onChange={this.handleChange} />
+            <p className='light small'>At least 8 characters, includes a number, uppercase character, and a symbol.</p>
+
+            <input type='password' id='passwordConfirmation'
+              placeholder='Password'
+              value={this.state.passwordConfirmation}
+              onChange={this.handleChange} />
+            <p className='light small'>Confirm your password here.</p>
+
+            <input type='submit' value='Sign up' />
+          </div><br />
+        </form>
+      </React.Fragment>
+    )
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.clearErrors();
+    if (this.state.name.length === 0 ) {
+      this.props.addError("Name is required");
+      return;
+    }
+
+    if (this.state.email.length === 0 ) {
+      this.props.addError("Email is required");
+    }
+
+    if (!this.validatePassword(this.state.password)) {
+      this.props.addError("Password does not meet requirements");
+      return;
+    }
+
+    if (!this.confirmPassword()) {
+      this.props.addError("Passwords do not match");
+      return;
+    }
+
+    console.log(this.state);
+  }
+
+  validatePassword = password => {
+    const validPass = /^.*(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!#$%&?"]).*$/
+    return validPass.test(password);
+  }
+
+  confirmPassword = () => {
+    return this.state.password === this.state.passwordConfirmation;
+  }
+}
+
+export default RegistrationForm;
--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
+import RegistrationForm from '../components/RegistrationForm';
+
 class Signup extends Component {
+  state = {
+    errors: []
+  }
+
   render() {
     return (
-      <div>Signup</div>
+      <div>
+        <h1>Join Espresso.Log</h1>
+        <p className='light'>Store and track your espresso recipes more efficiently</p>
+      
+        <RegistrationForm addError={this.addError}
+          clearErrors={this.clearErrors}
+          errors={this.state.errors} />
+      </div>
     );
+  }
+
+  addError = error => {
+    this.setState(state => {
+      return {
+        errors: state.errors.concat(error)
+      }
+    });
+  }
+
+  clearErrors = () => {
+    this.setState({
+      errors: []
+    });
   }
 }
 
