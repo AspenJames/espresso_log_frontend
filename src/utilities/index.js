4d70891160dfe230942269657888ea772df58e70
Aspen James <aspenjames@tqca.org>
2018-11-05 20:17:05 -0800

    add utilities for generating fetch requests

---
+++ src/utilities/index.js
@@ -0,0 +1,25 @@
+const defaultHeaders = {
+  "Accept": "applicaton/json",
+  "Content-Type": "application/json"
+};
+
+const headers = () => {
+  const jwt = localStorage.getItem('phoenixAuthToken');
+  return { ...defaultHeaders, jwt };
+}
+
+export const httpPost = (url, data) => {
+  const body = JSON.stringify(data);
+
+  return fetch(url, {
+    method: "POST",
+    headers: headers(),
+    body: body
+  }).then(resp => resp.json());
+}
+
+export const httpGet = url => {
+  return fetch(url, {
+    headers: headers()
+  }).then(resp => resp.json());
+}
