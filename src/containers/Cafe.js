ef6d69333413aaf871479d9dd4faccb137dd0860
Aspen James <aspenjames@tqca.org>
2019-01-07 10:33:34 -0800

    refactor to use static getDerivedStateFromProps

--- src/containers/Cafe.js
+++ src/containers/Cafe.js
@@ -3,15 +3,28 @@ import React, { Component } from 'react';
 class Cafe extends Component {
 
   state = {
-    cafeId: parseInt(this.props.location.pathname.replace("/cafes/", ""))
+    cafeId: parseInt(this.props.location.pathname.replace("/cafes/", "")),
+    cafe: null
+  }
+
+  static getDerivedStateFromProps = (props, state) => {
+    if (props.cafes.cafes.length > 0) {
+      const cafe = props.cafes.cafes.find(c => c.id === state.cafeId);
+      return {
+        ...state,
+        cafe: cafe
+      }
+    } else {
+      return null
+    }
   }
   render() {
-    const cafe = this.props.cafes.cafes.find(c => c.id === this.state.cafeId);
-    return !cafe ? null : (
+    return !this.state.cafe ? null : (
       <div className='container'>
         <div className='card indigo col s12 z-depth-4'>
           <h3 className='darker'>Cafe Container</h3>
-          <h4 className='darker'>{cafe && cafe.name}</h4>
+          <h4 className='darker'>{this.state.cafe.name}</h4>
+          <p className='darker'>{this.state.cafe.address}</p>
         </div>
       </div>
     )
